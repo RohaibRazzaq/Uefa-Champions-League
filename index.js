ba29562c1570b9fa5a6a4ba1411f330264c25dc3
RohaibRazzaq <129880535+RohaibRazzaq@users.noreply.github.com>
2024-10-25 15:40:50 +0500

    Mid-term Submission

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 document.addEventListener('DOMContentLoaded', () => {
     const hamburger = document.getElementById('hamburger');
     const mobileMenu = document.getElementById('mobileMenu');
-    const closeBtn = document.getElementById('closeBtn');
 
     hamburger.addEventListener('click', () => {
-        mobileMenu.style.display = 'flex';
+        mobileMenu.classList.toggle('show');
+    });
+
+    window.addEventListener('click', (event) => {
+        if (!hamburger.contains(event.target) && !mobileMenu.contains(event.target)) {
+            mobileMenu.classList.remove('show');
+        }
     });
 
     closeBtn.addEventListener('click', () => {
         mobileMenu.style.display = 'none';
     });
 
-    window.addEventListener('click', (event) => {
-        if (event.target === mobileMenu) {
-            mobileMenu.style.display = 'none';
-        }
-    });
 });
